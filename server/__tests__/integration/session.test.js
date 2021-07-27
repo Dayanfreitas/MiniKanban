d192d6ff56923a4942facb6052ebd9035e9c1a0f
dayanFreitas <dayan.freitas@catolicasc.org.br>
2021-07-26 23:36:23 -0300

    auth

--- server/__tests__/integration/session.test.js
+++ server/__tests__/integration/session.test.js
@@ -41,15 +41,30 @@ describe("Authenticated", () => {
         expect(response.status).toBe(401)
     })
 
-    // it("should be able to acess private routes when authenticated", async () => {
-    //     const user = await factory.create('User', { password: validPassword })
-    //     const response = await request(app)
-    //         .post('/sessions')
-    //         .set('Authorization', `Bearer ${user.get}`)
-    //         // .send({
-    //         //     email: user.email,
-    //         //     password: validPassword
-    //         // })
-    //     // expect(response.body).toHaveProperty("token")
-    // })
+    it("should be able to acess private routes when authenticated", async () => {
+        const user = await factory.create('User', { password: validPassword })
+        
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer ${user.generateToken()}`)
+
+        expect(response.status).toBe(200);
+    })
+  
+    it("should not be able to acess private routes it fout jwt token", async () => {
+        const response = await request(app)
+            .get('/dashboard')
+
+        expect(response.status).toBe(401);
+    })
+
+    it("should not be able to access private routes with invalid jwt token", async () => {
+        const user = await factory.create('User', { password: validPassword })
+        
+        const response = await request(app)
+            .get('/dashboard')
+            .set('Authorization', `Bearer 1232`)
+
+        expect(response.status).toBe(401);
+    })
 })
